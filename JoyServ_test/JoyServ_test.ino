27337297bc6ef5c5042dbe5a4b29feb1c84de9d0
cguanche1 <carlos.guanche@yale.edu>
2019-12-12 05:08:00 -0500

    Do I know how Git works?

--- JoyServ_test/JoyServ_test.ino
+++ JoyServ_test/JoyServ_test.ino
@@ -2,38 +2,53 @@
 
 #define NOISE 25;
 
-Servo myServo1;
-int pos1 = 0;
+Servo hServ;
+int hpos = 60;
 
-int servoPin1 = 26;
+int hPin = 17;
 
-Servo myServo2;
-int pos2 = 0;
+Servo vServ;
+int vpos = 23;
 
-int servoPin2 = 27;
+int vPin = 5;
 
-int joyxPin = 34;
+int joyxPin = 25;
 float xLow = 0;
 float xHigh = 0;
 
 
-int joyyPin = 35;
+int joyyPin = 26;
 float yLow = 0;
 float yHigh = 0;
 
+int joyButPin = 33;
+int joyLedPin = 18;
+
+int oneButPin = 34;
+int oneLedPin = 23;
+
+int twoButPin = 35;
+int twoLedPin = 22;
+
+int thrButPin = 32;
+int thrLedPin = 1;
+
+
+int lasLedPin = 15;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(servoPin1,OUTPUT);
-  pinMode(servoPin2,OUTPUT);
+  pinMode(hPin,OUTPUT);
+  pinMode(vPin,OUTPUT);
 
-  myServo1.setPeriodHertz(50);
-  myServo1.attach(servoPin1);
+  hServ.setPeriodHertz(50);
+  hServ.attach(hPin);
 
-  myServo2.setPeriodHertz(50);
-  myServo2.attach(servoPin2);
+  vServ.setPeriodHertz(50);
+  vServ.attach(vPin);
 
-  myServo1.write(pos1);
-  myServo2.write(pos2);
+  hServ.write(hpos);
+  vServ.write(vpos);
   
   pinMode(joyxPin,INPUT);
   pinMode(joyyPin,INPUT);
@@ -48,6 +63,31 @@ void setup() {
   yLow = curY - NOISE;
   yHigh = curY + NOISE;
 
+  pinMode(joyButPin, INPUT_PULLUP);
+  pinMode(joyLedPin, OUTPUT);
+  
+  digitalWrite(joyLedPin, HIGH);
+
+  pinMode(oneButPin, INPUT_PULLUP);
+  pinMode(oneLedPin, OUTPUT);
+
+  digitalWrite(oneLedPin,LOW);  
+
+  pinMode(twoButPin, INPUT_PULLUP);
+  pinMode(twoLedPin, OUTPUT);
+
+  digitalWrite(twoLedPin, HIGH);
+
+  pinMode(thrButPin, INPUT_PULLUP);
+  pinMode(thrLedPin, OUTPUT);
+
+  digitalWrite(thrLedPin,HIGH);
+
+  pinMode(lasLedPin, OUTPUT);
+
+  digitalWrite(lasLedPin,HIGH);
+
+  
   Serial.begin(9600);
   
 }
@@ -71,33 +111,33 @@ void loop() {
   }
 
   if(y > yHigh){
-    yAdd = 1;
-  }
-  if(y < yLow){
     yAdd = -1;
   }
-
-  if(xAdd == 1 && pos1 <180){
-    pos1 += xAdd;
-  }
-  if(xAdd == -1 && pos1 > 0){
-    pos1 += xAdd;
+  if(y < yLow){
+    yAdd = 1;
   }
 
-  if(yAdd == 1 && pos2 < 180){
-    pos2 += yAdd;
+  if(xAdd == 1 && hpos < 82){
+    hpos += xAdd;
   }
-  if(yAdd == -1 && pos2 > 0){
-    pos2 += yAdd;
+  if(xAdd == -1 && hpos > 36){
+    hpos += xAdd;
   }
 
-  myServo1.write(pos1);
+  if(yAdd == 1 && vpos < 48){
+    vpos += yAdd;
+  }
+  if(yAdd == -1 && vpos > 0){
+    vpos += yAdd;
+  }
+
+  hServ.write(hpos);
   delay(20);
-  myServo2.write(pos2);
+  vServ.write(vpos);
   delay(20);
 
-  Serial.print("pos1: " + String(pos1) + " x: " + String(x));
+  Serial.print("hpos: " + String(hpos));
 
-  Serial.print("  pos2: " + String(pos2) + " y: " + String(y) + "\n");
+  Serial.print("  vpos: " + String(vpos) + "\n");
   
 }
